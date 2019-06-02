a261747750492d8d54ace0d693d6501df447d7ee
Olga Rosas <olga.rosas@gmail.com>
2019-06-02 13:08:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,6 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   end
 
 def printer
-  batch_badge_creator
-  assign_rooms
+  puts batch_badge_creator
+  puts assign_rooms
 end
