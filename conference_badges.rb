3b7e8d10159a3948d8961e78b5005b5b8eceaee1
Olga Rosas <olga.rosas@gmail.com>
2019-06-02 13:32:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speakers)
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+#speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badge_messages = []
   speakers.each do |name|
     badge_messages << badge_maker(name)
@@ -11,9 +11,9 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
     return badge_messages
 end
 
-def assign_rooms
+def assign_rooms(speakers)
 rooms = [1, 2, 3, 4, 5, 6, 7]
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+#speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   room_assignment = []
   speakers.each_with_index do |speakers, index|
   room_assignment << "Hello, #{speakers}! You'll be assigned to room #{index + 1}!"
