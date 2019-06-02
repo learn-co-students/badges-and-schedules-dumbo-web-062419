c4ca08ec2099fdd389095dd71f62bab51518af0b
Olga Rosas <olga.rosas@gmail.com>
2019-06-02 12:31:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(speakers)
+  badge_messages = []
+  speakers.collect {|speakers| badge_maker(name)}
+    # message = badge_maker(speaker)
+    # badge_messages << message
+   end
+   return badge_messages
+end
+
+def assign_rooms
+  puts "Hello, #{name}! You'll be assigned to room #{room_number}!"
+end
+
+def printer
+  puts #the list of badges and room assignments
+end
