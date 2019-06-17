1b0e31d06efc7facf45cead5d97bbcb4f4207ac6
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-17 23:00:17 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
+require 'pry'
+
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    "Hello, my name is #{name}."
+  end
+end
+   def assign_rooms(list)
+     i = 0
+     list.each do |list_item |
+       i += 1
+      "Hello, #{list_item}! You'll be assigned to room #{i}"
+     end
+   end
+   
